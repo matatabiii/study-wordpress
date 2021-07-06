b1401cb7d84a08fab62ed96ea437f08de494ae88
matatabiii <27771489+matatabiii@users.noreply.github.com>
2021-07-06 22:12:10 +0900

    add wp_head

--- wp/wp-content/themes/wp-study/includes/header.php
+++ wp/wp-content/themes/wp-study/includes/header.php
@@ -6,6 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>トップページ - WordPressの集い</title>
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/main.css">
+
+  <?php wp_head(); ?>
 </head>
 <body>
 
